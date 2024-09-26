cfaf95acd026627fbac3e939f2377fd5a631a8b7
ardhikakrsh <ardhikakrishna@gmail.com>
2024-09-27 00:31:07 +0700

    feat: delete

--- resources/views/components/nav-link.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['active' => false])
-
-<a {{ $attributes->class([
-        'bg-gray-900' => $active,
-        'text-gray-300 hover:bg-gray-700 hover:text-white' => !$active,
-        'rounded-md px-3 py-2 text-sm font-medium text-white'
-    ]) }} aria-current="{{ $active ? 'page' : false }}">
-    {{ $slot }}
-</a>
