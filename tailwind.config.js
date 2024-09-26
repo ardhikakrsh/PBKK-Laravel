a84e5b56a68d7de097a0d18d02ff83e659eaa17e
ardhikakrsh <ardhikakrishna@gmail.com>
2024-09-27 00:08:45 +0700

    feat: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ export default {
             fontFamily: {
                 sans: ["Inter var", ...defaultTheme.fontFamily.sans],
             },
+            backgroundImage: {
+                login1: "url('assets/img/ls2.jpg')",
+            },
         },
     },
     plugins: [require("@tailwindcss/aspect-ratio"), require("flowbite/plugin")],
